4514494beaac8a215b5a8932d795e31e52e4f6f8
SavoeurnCH <94032467+SavoeurnCH@users.noreply.github.com>
2026-09-08 17:52:22 +0700

    Fix Spelling Title (remove ទៅ)

--- src/components/checkin/SectionHeading.vue
+++ src/components/checkin/SectionHeading.vue
@@ -2,6 +2,6 @@
 
 <template>
   <h2 class="text-center text-sm font-semibold text-gray-800">
-    <span lang="km">សូមផ្តល់ព័ត៌មានទៅ</span> - Please Provide Information
+    <span lang="km">សូមផ្តល់ព័ត៌មាន</span> - Please Provide Information
   </h2>
 </template>
